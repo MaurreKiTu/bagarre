00aef92c2fb1a25013cb81f2b616b7711b9f2a60
MaurreKiTu <alexandre.sabouni@gmail.com>
2024-03-01 16:55:22 +0100

    cleaned components

--- src/app/routes/resources.route.ts
+++ src/app/routes/resources.route.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ResourcesComponent } from '../components/resources.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ResourcesComponent],
-  template: `
-    <resources-comp/>
-  `
+  imports: [CommonModule],
+  template: ` resources `,
 })
 export default class ResourcesRoute {
   title = 'resources-route';
